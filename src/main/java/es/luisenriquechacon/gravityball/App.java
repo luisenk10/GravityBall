6d89502211173217f9177e36edbddc7d3592e35f
usuario <usuario@192.168.15.107>
2020-02-03 10:31:23 +0100

    Tiene Fondo y la bola

--- src/main/java/es/luisenriquechacon/gravityball/App.java
+++ src/main/java/es/luisenriquechacon/gravityball/App.java
@@ -2,8 +2,9 @@ package es.luisenriquechacon.gravityball;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -13,13 +14,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Pane root = new Pane();
+        var scene = new Scene(root, 800, 470);
         stage.setScene(scene);
         stage.show();
+        
+        Image image1 = new Image(getClass().getResourceAsStream("/imagenes/fondo.png"));
+        ImageView imageView1 = new ImageView(image1);
+        root.getChildren().add(imageView1);
+        Image image2 = new Image(getClass().getResourceAsStream("/imagenes/bola.gif"));
+        ImageView imageView2 = new ImageView(image2);
+        root.getChildren().add(imageView2);
+        
+    // Movimiento Bola
+        
     }
 
     public static void main(String[] args) {
